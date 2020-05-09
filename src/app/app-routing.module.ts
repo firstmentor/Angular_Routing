0e52674129bd55480a8be61572b0ba324ab0c916
firstmentor <jainvikas887@gmail.com>
2020-05-09 16:31:41 +0530

    ff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,55 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContentComponent } from './content/content.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyhtmlComponent } from './myhtml/myhtml.component';
+import { CssComponent } from './css/css.component';
+import { BoostrapComponent } from './boostrap/boostrap.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+
+  },
+
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    children:[
+      {path:'',component:AboutComponent},
+      {path:'html',component:MyhtmlComponent},
+      {path:'css',component:CssComponent},
+      {path:'bootstrap',component:BoostrapComponent}
+    ],
+    
+  },
+  {
+    path:'blog',
+    component:BlogComponent
+  },
+  {
+    path:'content',
+    component:ContentComponent
+  },
+  
+
+//Wildcard Route sab se neeche rakhate hai
+  {
+    path:'**',
+    component:PageNotFoundComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyhtmlComponent } from './myhtml/myhtml.component';
+import { CssComponent } from './css/css.component';
+import { BoostrapComponent } from './boostrap/boostrap.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContentComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
+    MyhtmlComponent,
+    CssComponent,
+    BoostrapComponent,
+    
   ],
   imports: [
     BrowserModule,
